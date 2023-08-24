bd4a8891238256484a59fa9ac93c7c90b52e6ca2
bp289 <birajpanta112@gmail.com>
2023-08-24 13:17:41 +0100

    practiced around with styling and flatlist

--- components/Bosses.tsx
+++ components/Bosses.tsx
@@ -1,15 +1,25 @@
-import * as React from 'react';
-import {Text, View} from 'react-native';
+import React, {useMemo} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 import {useQuery} from '@apollo/client';
 import {GET_BOSS} from '../GraphQL/queries';
 
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {HomeStackParams} from '../screens/Home';
+import type {HomeStackParams} from '../screens/Home';
 
 type Props = NativeStackScreenProps<HomeStackParams, 'Bosses'>;
 
 export default function Bosses({navigation}: Props): JSX.Element {
   const {loading, error, data} = useQuery(GET_BOSS);
+  const {boss} = useMemo(() => {
+    return data ? data : [];
+  }, [data]);
 
   if (loading) {
     return <Text>'Loading...'</Text>;
@@ -18,8 +28,52 @@ export default function Bosses({navigation}: Props): JSX.Element {
     return <Text>`Error! ${error.message}`</Text>;
   }
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>{JSON.stringify(data)}</Text>
-    </View>
+    <FlatList
+      style={styles.container}
+      data={boss}
+      renderItem={({item}) => (
+        <TouchableOpacity>
+          {item.image ? (
+            <ImageBackground
+              blurRadius={2}
+              style={styles.thumbnail}
+              source={{
+                uri: item.image,
+              }}>
+              <Text style={styles.textStyle}>{item.name}</Text>
+            </ImageBackground>
+          ) : (
+            <ImageBackground
+              blurRadius={8}
+              style={styles.thumbnail}
+              source={{
+                uri: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/er_abductor_virgin.png',
+              }}>
+              <Text style={styles.textStyle}>{item.name}</Text>
+            </ImageBackground>
+          )}
+        </TouchableOpacity>
+      )}
+      keyExtractor={item => item.id}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  thumbnail: {
+    height: 100,
+    flex: 1,
+    justifyContent: 'center',
+    resizeMode: 'cover',
+  },
+  textStyle: {
+    fontFamily: 'Mantinia Regular',
+    fontSize: 20,
+    color: '#F9DF99',
+    marginTop: 40,
+    marginLeft: 10,
+  },
+});
